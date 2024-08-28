769905024c15095cd0ead77c1c69a42703f7e444
paillaabhinaya <126546867+paillaabhinaya@users.noreply.github.com>
2024-08-28 12:28:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from langchain.agents import initialize_agent,AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
+## code
+####
 
 ## Arxiv and wikipedia Tools
 arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
@@ -15,7 +17,6 @@ api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
 
 search=DuckDuckGoSearchRun(name="Search")
-####
 
 st.title("🔎 LangChain - Chat with search")
 """
