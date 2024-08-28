9bac6e723a80339f0505272eca5938a7c6c492f2
paillaabhinaya <126546867+paillaabhinaya@users.noreply.github.com>
2024-08-28 13:57:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from langchain.agents import initialize_agent,AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
-## code
-####
 
 ## Arxiv and wikipedia Tools
 arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
