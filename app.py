c16d36f6d6bcf63c5d87f6669e40c602a4f8d1bf
paillaabhinaya <126546867+paillaabhinaya@users.noreply.github.com>
2024-08-28 12:24:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
 
 search=DuckDuckGoSearchRun(name="Search")
-
+####
 
 st.title("🔎 LangChain - Chat with search")
 """
